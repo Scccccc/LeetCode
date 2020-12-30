34f19b5daddfa0905928f19a0b16a2720b5e4ea7
Scccccc <76455906+Scccccc@users.noreply.github.com>
2020-12-30 21:45:23 +0800

    Create test.py

---
+++ 0003.Longest-Substring-Without-Repeating-Characters/test.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result=0
+        temp=''
+        for item in s:
+            if item not in temp:
+                temp += item
+            else:
+                if len(temp)>result:
+                    result=len(temp)
+                temp = temp[temp.index(item) + 1:]
+                temp += item
+        return max(len(temp),result)
